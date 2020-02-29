b03ee03b9fa83e5585aa156d67c2c39e1710c4c8
Charlie Thao <cthao309@gmail.com>
2020-02-29 12:34:18 -0800

    finished recursion module function factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,16 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if(n === 0) {
+		return 1;
+	}
+
+	if(n < 0) {
+		return null;
+	}
+
+	let fact = n * factorial(n-1);
+	return fact;
 };
 
 // 2. Compute the sum of an array of integers.
